f40e19aceb7dcd31b24f1ace611a63e74aa6c7b5
Дмитрий С. Демидовец <ddemidovec@avtoprom.by>
2023-01-03 12:22:43 +0200

    space_inveders.py

--- HW7/GAME/space_inviders.py
+++ HW7/GAME/space_inviders.py
@@ -89,15 +89,13 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
 # Game Loop
 running = True
 while running:
- 
-# RGB = Red, Green, Blue
+ # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
- 
-# Background Image
-screen.blit(background, (0, 0))
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        running = False
+ # Background Image
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
  
 # if keystroke is pressed check whether its right or left
 if event.type == pygame.KEYDOWN:
