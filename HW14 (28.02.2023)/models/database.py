19df8ea9c6cc53079b57ddab3d473f0b52e5214a
Дмитрий С. Демидовец <ddemidovec@avtoprom.by>
2023-03-20 15:36:40 +0300

    database.py

--- HW14 (28.02.2023)/models/database.py
+++ HW14 (28.02.2023)/models/database.py
@@ -4,3 +4,13 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 DATABASE_NAME = 'computer.firm.db'
+
+engine = creat_engine(f'ssqlite:///{DATABASE_NAME}')
+
+class Base(DeclarativeBase)
+    pass
+
+
+def create_database():
+    Base.metadata.create_all(engine)
+    
