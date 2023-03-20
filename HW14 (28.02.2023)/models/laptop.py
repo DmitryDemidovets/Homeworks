42a474ea58ce073774f6b1ee29b99d3e5258c3cb
Дмитрий С. Демидовец <ddemidovec@avtoprom.by>
2023-03-20 16:09:44 +0300

    laptop.py

---
+++ HW14 (28.02.2023)/models/laptop.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.database import Base
+
+
+class Laptop(Base):
+    __tablename__ = 'laptops'
+
+    id = Column(Integer, primary_key=True, index = True)
+    model_id = Column(Integer, ForeignKey('products_id'))
+    speed = Column (Integer)
+    ram = Column (Integer)
+    cd = Column (String)
+    hd = Column(Integer)
+    price = Column(Integer)
+    screen = Column(Integer)
+
+
+    def __init__(self, model_id: int, sped: int, ram: int, cd: str, hd: int, price: int, screen: int):
+        self.model_id = model_id
+        self.speed = speed
+        self.ram = ram
+        self.cd = cd
+        self.hd = hd
+        self.price = price
+        self.screen = screen
