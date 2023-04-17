566a6929249d9259b4eda0c1f1a74a7679fd1e0e
Дмитрий С. Демидовец <ddemidovec@avtoprom.by>
2023-04-17 19:31:52 +0300

    HW18

---
+++ CW13.04/script.js
@@ -0,0 +1,9 @@
+console.log("Hi");
+const HeaderBtn= document.getElementById("header__btn");
+const MainBtn = document.querySelector("#main__btn");
+const BodyStyle = document.getElementById("body__style");
+const MainDiv = document.querySelector(".main__div");
+
+HeaderBtn.addEventListener("click", () => {if (document.body.id==="body__style")})
+{(document.body.id="body__style_dark")}
+else
